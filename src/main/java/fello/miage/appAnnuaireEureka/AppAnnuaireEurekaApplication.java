c86daa5c756d33d5b60d0399d25ff5dc688e0e77
ib-fello <ibrahimafello2017@gmail.com>
2026-04-11 20:37:44 +0200

    feat: initial commit

---
+++ src/main/java/fello/miage/appAnnuaireEureka/AppAnnuaireEurekaApplication.java
@@ -0,0 +1,15 @@
+package fello.miage.appAnnuaireEureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class AppAnnuaireEurekaApplication {
+	public static void main(String[] args) {
+
+        SpringApplication.run(AppAnnuaireEurekaApplication.class, args);
+	}
+
+}
